337c212027ff8fc823e3a041b270ba943af4830a
Thomas Polasek <lemon97@gmail.com>
2013-11-01 19:58:54 -0600

    checkpoint

--- blox.cpp
+++ blox.cpp
@@ -10,7 +10,7 @@
 #include <string>
 #include <iostream>
 #include <boost/foreach.hpp>
-#include <assert.h>  
+#include <assert.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -36,78 +36,84 @@ const int MAIN_LOOP_DELAY =   10;
 
 
 class Cell{
-    public:
-        unsigned int is_frozen;
-        unsigned int is_herd;
+public:
+    unsigned int is_frozen;
+    unsigned int is_herd;
 };
 
 class CellMatrix {
-    private:
-        Cell * matrix;
-        unsigned int width;  //unit cell
-        unsigned int height; //unit cell
+private:
+    Cell * matrix;
+    unsigned int width;  //unit cell
+    unsigned int height; //unit cell
     
-    public:
-        CellMatrix(unsigned int _width, unsigned int _height){
-            width = _width;
-            height = _height;
-            matrix = new Cell[width*height]();
-            
-            unsigned i,j;
-            for(j = 0; j < height; j++){
-                for(i = 0; i < width; i++){
-                    (getCellIndex(i,j))->is_frozen = 0;
-                }
+public:
+    CellMatrix(unsigned int _width, unsigned int _height){
+        width = _width;
+        height = _height;
+        matrix = new Cell[width*height]();
+        
+        unsigned i,j;
+        for(j = 0; j < height; j++){
+            for(i = 0; i < width; i++){
+                (getCellIndex(i,j))->is_frozen = 0;
             }
         }
+    }
     
-        // pixel units
-        static unsigned int getCellSize() {return 10;}
+    // pixel units
+    static unsigned int getCellSize() {return 10;}
     
-        unsigned int getWidth () {return width;}
-        unsigned int getHeight () {return height;}
+    unsigned int getWidth () {return width;}
+    unsigned int getHeight () {return height;}
     
-        Cell * getCellIndex(unsigned int x, unsigned int y){
-            if (y >= 0 && x < width && y >= 0 && y < height){
-                return &(matrix[x + y*width]);
-            }
-            else{
-                return NULL;
-            }
+    Cell * getCellIndex(unsigned int x, unsigned int y){
+        if (y >= 0 && x < width && y >= 0 && y < height){
+            return &(matrix[x + y*width]);
         }
-    
-        ~CellMatrix(){
-            delete[] matrix;
+        else{
+            return NULL;
         }
+    }
+    
+    ~CellMatrix(){
+        delete[] matrix;
+    }
 };
 
 class Window {
-    private:
-        unsigned int x;
-        unsigned int y;
+private:
+    unsigned int x;
+    unsigned int y;
     
-        unsigned int width;
-        unsigned int height;
+    unsigned int width;
+    unsigned int height;
     
-        unsigned int maxScrollWidth;
-        unsigned int maxScrollHeight;
+    unsigned int maxScrollWidth;
+    unsigned int maxScrollHeight;
     
-    public:
-        Window (int _x,int _y, int _width, int _height, int _maxScrollWidth, int _maxScrollHeight){
-            x = _x;
-            y = _y;
-            width = _width;
-            height = _height;
-            maxScrollWidth = _maxScrollWidth;
-            maxScrollHeight = _maxScrollHeight;
-            
-        }
+    SDL_Surface* surface;
+    
+public:
+    Window (int _x,int _y, int _width, int _height, int _maxScrollWidth, int _maxScrollHeight){
+        x = _x;
+        y = _y;
+        width = _width;
+        height = _height;
+        maxScrollWidth = _maxScrollWidth;
+        maxScrollHeight = _maxScrollHeight;
+        
+        SDL_Init( SDL_INIT_EVERYTHING );
+        surface = SDL_SetVideoMode( width, height, 24, SDL_SWSURFACE );
+    }
     
     unsigned int getX () {return x;}
     unsigned int getY () {return y;}
     
     unsigned int getWidth () {return width;}
     unsigned int getHeight () {return height;}
+    
+    SDL_Surface* getSurface () {return surface;}
     
     void scrollHorizonally(int amount) {
         x += amount;
@@ -120,24 +126,58 @@ class Window {
         if(y < 0) y = 0;
         if(y >= maxScrollHeight) y = maxScrollHeight-1;
     }
+    
+    void renderCells(CellMatrix & cells){
+        
+        //blank
+        SDL_Rect rect = {0,0,getWidth(), getHeight()};
+        SDL_FillRect(getSurface(), &rect, COLOR_WHITE);
+        
+        int cell_pixel_width = CellMatrix::getCellSize();
+        int cell_pixel_height = CellMatrix::getCellSize();
+        
+        unsigned int i,j;
+        
+        for(j = 0; j < cells.getHeight(); j++){
+            for(i = 0; i < cells.getWidth(); i++){
+                
+                SDL_Rect rect = {i*cell_pixel_width,
+                                 j*cell_pixel_height,
+                                 cell_pixel_width,
+                                 cell_pixel_height};
+                
+                rect.x -= getX();
+                rect.y -= getY();
+                
+                if((cells.getCellIndex(i,j))->is_frozen){
+                    SDL_FillRect(getSurface(), &rect, COLOR_BLACK);
+                    rect.x += 1;
+                    rect.y += 1;
+                    rect.w -= 2;
+                    rect.h -= 2;
+                    SDL_FillRect(getSurface(), &rect, COLOR_WHITE);
+                }
+                else{
+                    SDL_FillRect(getSurface(), &rect, COLOR_WHITE);
+                }
+            }
+        }
+    }
+    void renderFinish(){
+         SDL_Flip(getSurface() );
+    }
 };
 
-typedef struct {
-    unsigned int width, height;
-	SDL_Surface* surface;
-} Screen;
-
-static Screen screen;
 static CellMatrix cells(CELL_WIDTH,CELL_HEIGHT);
-static Window mainWindow(0,0,100,100,3000,3000);
+static Window * mainWindow;
 
 
 int calculate_Cell_X(int x){
-	return  x/(screen.width/cells.getWidth());
+	return  x/(mainWindow->getWidth()/cells.getWidth());
 }
 
 int calculate_Cell_Y(int y){
-	return y/(screen.height/cells.getHeight());
+	return y/(mainWindow->getHeight()/cells.getHeight());
 }
 
 void toggle_Cell(int mouse_x, int mouse_y){
@@ -147,64 +187,17 @@ void toggle_Cell(int mouse_x, int mouse_y){
 }
 
 
-void render_Screen(){
-	SDL_Rect rect = {0,0,screen.width,screen.height};
-	SDL_FillRect(screen.surface, &rect, COLOR_WHITE);
-	
-	
-	int cell_pixel_width = screen.width/cells.getWidth();
-	int cell_pixel_height = screen.height/cells.getHeight();
-	
-	unsigned int i,j;
-	
-	for(j = 0; j < cells.getHeight(); j++){
-		for(i = 0; i < cells.getWidth(); i++){
-			
-			SDL_Rect rect = {i*cell_pixel_width,j*cell_pixel_height,
-							 cell_pixel_width,cell_pixel_height};
-            
-            rect.x -= mainWindow.getX();
-            rect.y -= mainWindow.getY();
-
-			
-			if((cells.getCellIndex(i,j))->is_frozen){
-                SDL_FillRect(screen.surface, &rect, COLOR_BLACK);
-                rect.x += 1;
-                rect.y += 1;
-                rect.w -= 2;
-                rect.h -= 2;
-				SDL_FillRect(screen.surface, &rect, COLOR_WHITE);
-			}
-			else{
-                SDL_FillRect(screen.surface, &rect, COLOR_WHITE);
-			}
-		}
-	}
-	
-    SDL_Flip( screen.surface );
-}
-
 void exit_Game(){
 	SDL_Quit();
 	exit(0);
 }
 
 int main( int argc, char* args[] ){
-
-    std::string hello( "Hello, world!" );
-    BOOST_FOREACH( char ch, hello )
-    {
-        std::cout << ch;
-    }
-	
-	srand ( time(NULL) );
-	
-    SDL_Init( SDL_INIT_EVERYTHING );
-
-	screen.width = SCREEN_WIDTH;
-	screen.height = SCREEN_HEIGHT;
-    screen.surface = SDL_SetVideoMode( screen.width, screen.height, 24, SDL_SWSURFACE );
-	
+    
+    srand ( time(NULL) );
+    
+    mainWindow = new Window(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,3000,3000);
+    
 	int is_game = 1;
     
 	while(is_game){
@@ -214,25 +207,26 @@ int main( int argc, char* args[] ){
 				case SDL_MOUSEBUTTONDOWN:
 					toggle_Cell(event.button.x, event.button.y);
 					break;
-                
+                    
                 case SDL_KEYDOWN:
                     if(event.key.keysym.sym == SDLK_LEFT)
-                        mainWindow.scrollHorizonally(-1*SCROLL_AMOUNT);
+                        mainWindow->scrollHorizonally(-1*SCROLL_AMOUNT);
                     else if(event.key.keysym.sym == SDLK_RIGHT)
-                        mainWindow.scrollHorizonally(SCROLL_AMOUNT);
+                        mainWindow->scrollHorizonally(SCROLL_AMOUNT);
                     else if(event.key.keysym.sym == SDLK_UP)
-                        mainWindow.scrollVertically(-1*SCROLL_AMOUNT);
+                        mainWindow->scrollVertically(-1*SCROLL_AMOUNT);
                     else if(event.key.keysym.sym == SDLK_DOWN)
-                        mainWindow.scrollVertically(SCROLL_AMOUNT);
+                        mainWindow->scrollVertically(SCROLL_AMOUNT);
                     break;
-				
+                    
 				case SDL_QUIT:
 					is_game = 0;
 					break;
 			}
 		}
         
-		render_Screen();
+		mainWindow->renderCells(cells);
+        mainWindow->renderFinish();
 		SDL_Delay( MAIN_LOOP_DELAY );
 	}
 	printf("You lost the game.\nNoob!\n");
